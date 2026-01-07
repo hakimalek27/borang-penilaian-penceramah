fc736f276bbc5e566c974b7a9ca112c48bd34369
hakimalek27 <hakimalek27@gmail.com>
2026-01-07 15:03:29 +0800

    fix: move floating button to 50rem on mobile (+25rem)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -803,7 +803,7 @@
 	/* Floating Scroll Assist */
 	.scroll-assist {
 		position: fixed;
-		bottom: calc(25rem + env(safe-area-inset-bottom, 0px));
+		bottom: calc(50rem + env(safe-area-inset-bottom, 0px));
 		right: 1rem;
 		width: 68px;
 		height: 68px;
